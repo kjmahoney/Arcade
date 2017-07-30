283d3a443fac5a2ef30157b8a850d44f0491350e
Kevin Mahoney <kjmahoney13@gmail.com>
2017-07-30 19:32:31 -0400

    add sound

--- js/game.js
+++ js/game.js
@@ -17,6 +17,17 @@ BasicGame.Game.prototype = {
     this.load.spritesheet('player', 'assets/player.png', 64, 64);
     this.load.spritesheet('whiteEnemy', 'assets/shooting-enemy.png', 32, 32);
     this.load.spritesheet('boss', 'assets/boss.png', 93, 75);
+
+    this.load.audio('explosion',
+                    ['assets/explosion.ogg', 'assets/explosion.wav']);
+    this.load.audio('playerExplosion',
+                    ['assets/player-explosion.ogg', 'assets/player-explosion.wav']);
+    this.load.audio('enemyFire',
+                    ['assets/enemy-fire.ogg', 'assets/enemy-fire.wav']);
+    this.load.audio('playerFire',
+                    ['assets/player-fire.ogg', 'assets/player-fire.wav']);
+    this.load.audio('powerUp',
+                    ['assets/powerup.ogg', 'assets/powerup.wav']);
   },
 
   create: function () {
